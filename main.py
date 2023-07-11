11c191bf009d8ff2db23c7b806df4fd20573c5ba
Алина <alina.khalatian@gmail.com>
2023-07-12 00:20:16 +0300

    Первый

---
+++ main.py
@@ -0,0 +1,15 @@
+"""def strcounter(s):
+    for sym in set(s):
+        count=0
+        for sub_sym in s:
+            if sym==sub_sym:
+                count+=1
+        print(sym,count)
+strcounter('gdfdfgg')"""
+def strcounter(s):
+    syms_counter={}
+    for sym in s:
+        syms_counter[sym]=syms_counter.get(sym,0)+1
+    for sym,count in syms_counter.items():
+        print(sym,count)
+strcounter('gdfdfgg')
